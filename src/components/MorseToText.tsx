f6b6a5f07854060e4aa00a5e2b27f205c28d293c
Dominik Liebler <dominik.liebler@flyeralarm.com>
2021-05-09 12:39:13 +0200

    copy result to clipboard added

--- src/components/MorseToText.tsx
+++ src/components/MorseToText.tsx
@@ -17,6 +17,13 @@ const MorseToText = (props: MorseToTextProps) => {
     props.onChange && props.onChange(newValue);
   };
 
+  const onCopy = () => {
+    if (morse === "") return;
+
+    navigator.clipboard.writeText(morseToText(morse));
+    alert("copied to clipboard");
+  };
+
   return (
     <div className="MorseToText">
       <h1>Morse-to-text</h1>
@@ -29,24 +36,26 @@ const MorseToText = (props: MorseToTextProps) => {
         autoFocus
       ></input>
 
-      <p className="output">{morseToText(morse).toLowerCase()}</p>
+      <div className="output-section">
+        <p className="output">{morseToText(morse).toLowerCase()}</p>
 
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-          />
-        </svg>
-      </a>
+        <button onClick={onCopy}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/TextToMorse.tsx
+++ src/components/TextToMorse.tsx
@@ -5,6 +5,7 @@ import "./TextToMorse.css";
 interface TextToMorseProps {
   text?: string;
   onChange?: (text: string) => void;
+  onClipboard?: (text: string) => void;
 }
 
 const TextToMorse = (props: TextToMorseProps) => {
@@ -15,6 +16,13 @@ const TextToMorse = (props: TextToMorseProps) => {
 
     setText(newValue);
     props.onChange && props.onChange(newValue);
+  };
+
+  const onCopy = () => {
+    if (text === "") return;
+
+    navigator.clipboard.writeText(textToMorse(text));
+    alert("copied to clipboard");
   };
 
   return (
@@ -29,24 +37,26 @@ const TextToMorse = (props: TextToMorseProps) => {
         autoFocus
       ></input>
 
-      <p className="output">{textToMorse(text)}</p>
+      <div className="output-section">
+        <p className="output">{textToMorse(text)}</p>
 
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-          />
-        </svg>
-      </a>
+        <button onClick={onCopy}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
