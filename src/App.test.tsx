e533373ad8697b3c6ff7cb258866a67a9fa9bfde
Dominik Liebler <dominik.liebler@flyeralarm.com>
2021-05-10 14:05:10 +0200

    fix Redux context

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { Store } from "./app/store";
+import { Provider } from "react-redux";
 
 test("default route", () => {
-  const { container } = render(<App />);
+  const { container } = render(
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  );
   const textToMorse = container.querySelector(".TextToMorse");
 
   expect(textToMorse).toBeInTheDocument();
